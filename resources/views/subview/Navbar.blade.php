0400043fa409d467053fe3cc6a09a163bb9975fe
nazi sharifi <banonazisharifi@gmail.com>
2025-12-11 16:27:18 +0430

    styles

---
+++ resources/views/subview/Navbar.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+</head>
+<body class="w-full">
+    <div class="h-24 px-12 fixed w-full flex justify-between items-center bg-black text-white">
+        <img src="logo.png" alt="">
+        <ul class="flex items-center justify-center gap-4 text-xl font-bold">
+            <li>Home</li>
+            <li>About</li>
+            <li>Page</li>
+            <li>Music</li>
+        </ul>
+        <div class="flex items-center justify-center gap-4 text-xl text-white">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-headphones"></i>
+        </div>
+    </div>
+</body>
+</html>
