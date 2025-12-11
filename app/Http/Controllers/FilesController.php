6397e79d91aa6c91ae45bd3b445bd7895005e606
nazi sharifi <banonazisharifi@gmail.com>
2025-12-11 16:52:29 +0430

    added some files

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Files;
 use Illuminate\Http\Request;
 
 class FilesController extends Controller
 {
-    //
+    public function insert(){
+        $file = new Files();
+        return view('insert', compact('file'));
+    }
 }
